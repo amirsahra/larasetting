926eeebe7a0ef7ebc9de5c9124699f613d8e072f
amirsahra <amirhosein.sahra@gmail.com>
2024-01-01 12:25:12 +0330

    feat(package):  add method doc facade

--- src/Facades/Larasetting.php
+++ src/Facades/Larasetting.php
@@ -4,6 +4,15 @@ namespace Amirsahra\Larasetting\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Larasetting
+ *
+ * @method static mixed getSetting(string $key = null, mixed $default = null, bool $is_active = null)
+ * @method static mixed setSetting(string $key, mixed $value, bool $is_active = null)
+ *
+ * @package Amirsahra\Illustrator\Facade
+ * @see \Amirsahra\Larasetting\
+ */
 class Larasetting extends Facade
 {
     /**
