7c86e8bf5cc7785e326dc475bbe9694e29b2efbc
amirsahra <amirhosein.sahra@gmail.com>
2024-01-01 09:21:32 +0330

    feat(package): publish migration

--- src/LarasettingServiceProvider.php
+++ src/LarasettingServiceProvider.php
@@ -6,15 +6,16 @@ use Illuminate\Support\ServiceProvider;
 
 class LarasettingServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function boot(): void
     {
 
         $this->publishes([
             __DIR__ . '/../configs/larasetting.php' => config_path('larasetting.php'),
+            __DIR__ . '/../database/migrations' => database_path('migrations')
         ], 'larasetting');
     }
 
-    public function register()
+    public function register(): void
     {
         $this->mergeConfigFrom(__DIR__ . '/../configs/larasetting.php', 'larasetting');
     }
